af5ab1a1c577c5b35b92fc98c9ebe660b66c880a
Ido Leshem <35726594+Idoleshem@users.noreply.github.com>
2023-12-12 23:06:36 +0200

    Create check_if_text_is_ai_generated.py

---
+++ check_if_text_is_ai_generated.py
@@ -0,0 +1,57 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import torch
+import re
+from spellchecker import SpellChecker
+def average_amount_of_typos(text):
+    spell = SpellChecker()
+
+    # Remove punctuation signs from the text
+    text_cleaned = re.sub(r'[^\w\s]', '', text)
+
+    # Split the cleaned text into words and identify misspelled ones
+    words = text_cleaned.split()
+    misspelled = spell.unknown(words)
+    typo_count = len(misspelled)
+
+    # Calculate the average count of typos per word
+    word_count = len(words)
+    average_typos = typo_count / word_count if word_count > 0 else 0
+
+    return average_typos
+
+def get_perplexity(text):
+
+    # Load pre-trained GPT-2 model and tokenizer
+    model_id = "gpt2-large"
+    tokenizer = GPT2Tokenizer.from_pretrained(model_id)
+    model = GPT2LMHeadModel.from_pretrained(model_id)
+    model.eval()
+
+    # Tokenize the text
+    tokenized_text = tokenizer.encode(text, return_tensors='pt')
+
+    # Calculate perplexity using GPT-2
+    with torch.no_grad():
+      outputs = model(tokenized_text, labels=tokenized_text)
+      loss = outputs.loss
+      perplexity = torch.exp(loss)
+
+    return perplexity
+
+# Detect typos
+def check_if_text_is_ai_generated(text):
+
+    amount_of_typos = average_amount_of_typos(text)
+    perplexity = get_perplexity(text)
+
+    print(amount_of_typos)
+    print(f"Perplexity: {perplexity}")
+
+    if amount_of_typos > 0.05 or perplexity > 50:
+      print("text written by human")
+    else:
+      print("text is ai generated")
+
+
+check_if_text_is_ai_generated(text1)
+check_if_text_is_ai_generated(text2)
